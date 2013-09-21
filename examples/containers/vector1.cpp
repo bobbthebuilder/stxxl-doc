129005e7e31a4457ea30b85f3dbd164530f9c13e
Daniel Feist <daniel.feist@student.kit.edu>
2013-09-21 02:40:46 +0200

    modified vector documentation

--- examples/containers/vector1.cpp
+++ examples/containers/vector1.cpp
@@ -13,44 +13,44 @@
 //! [example]
 #include <iostream>
 #include <stxxl/vector>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
-{
-    // <value_type, page_size, number_of_pages, block_size, alloc_strategy, paging_strategy> 
-    typedef stxxl::VECTOR_GENERATOR<int, 4, 8, 1*1024*1024, stxxl::RC, stxxl::lru>::result vector_type;
+{ 
+  // template parameter <value_type, page_size, number_of_pages, block_size, alloc_strategy, paging_strategy> 
+  typedef stxxl::VECTOR_GENERATOR<int, 4, 8, 1*1024*1024, stxxl::RC, stxxl::lru>::result vector_type;  
+
+  vector_type V;
+  int counter = 0;
+
+  srand (time(NULL));
   
-    vector_type V;
-    int counter = 0;
+  // fill vector with random integers
+  for (size_t i = 0; i < 10000000; ++i) {
+    V.push_back(rand() % 100000);  
+  }
 
-    srand(903428594);
+  // return size    
+  std::cout << "size of V? " << V.size() << std::endl;
 
-    // fill vector with random integers
+  // random access operator[] to receive a const reference to the element at position n                
+  const vector_type &cv = V;
+  std::cout << "V[0]=" << cv.front() << " " << ", V[n]=" << cv.back() << ", V[1234]=" << cv[1234] << " " << std::endl;
 
-    for (size_t i = 0; i < 10000000; ++i) {
-        V.push_back(rand() % 10000);  
-    }
-
-    std::cout << "Size of vector V is: " << V.size() << std::endl;
-  
-    // iterate over vector 
-
-    vector_type::const_iterator iter = V.begin();
+  // iterate over vector from front to back and access each element
+  vector_type::const_iterator iter = V.begin();
  
-    for (size_t j = 0; j < V.size(); ++j) {
-        //std::cout << *iter << " ";     
-        if (*iter % 2 == 0) {  // is V's current element even?
-            ++counter;
-        }    
-        iter++;
-    }
+  for (size_t j = 0; j < V.size(); ++j) {
+    //std::cout << *iter << " ";  // uncomment to observe scanning    
+    if (*iter % 2 == 0) {  // is V's current element even?
+      ++counter;
+    }    
+    iter++;
+  }
    
-    std::cout << "Found " << counter << " even numbers in V" << std::endl;
+  std::cout << "found " << counter << " even numbers in V" << std::endl;  
 
-    // random access operator [] 
-
-    const vector_type &cv = V;
-    std::cout << cv[1234] << " ";
-
-    return 0;
+  return 0;
 }
 //! [example]
