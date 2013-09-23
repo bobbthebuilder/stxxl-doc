435b251b2dbffd63de98adffcae95f7df058f4c6
Daniel Feist <daniel.feist@student.kit.edu>
2013-09-23 17:42:16 +0200

    fixed stack example

--- examples/containers/stack1.cpp
+++ examples/containers/stack1.cpp
@@ -20,7 +20,7 @@ int main()
   //                     migrating_critical_size, allocation_strategy, size_type>
   typedef stxxl::STACK_GENERATOR<int, stxxl::external, stxxl::grow_shrink2, 4, 1*1024*1024, stxxl::RC>::result external_stack;
   
-  // prefetch/write pool with 10 blocks prefetching and 10 block write cache (> D is recommended)
+  // prefetch/write pool with 10 blocks prefetching and 10 block write cache 
   stxxl::read_write_pool<external_stack::block_type> pool(10, 10);
 
   // create a stack with prefetch aggressiveness of 0
@@ -28,21 +28,22 @@ int main()
    
   // grow-shrink routine: 1) push random values on stack and 2) pop all except the lowest value and start again
   stxxl::random_number32 random;
-  int number_of_elements = 128 * 1024 * 1024;
+  stxxl::int64 number_of_elements = 128 * 1024 * 1024;
 
-  for (int k = 0; k < 10; k++) {
-    for (int i = 0; i < number_of_elements; i++) 
-    {
-      my_stack.push(random());       
-    }
-    
-    // gives the hint that shrinking is imminent and always prefetche 5 buffers from now on
+   for (int k = 0; k < 10; k++) {
+    STXXL_MSG("push...");
+    for (stxxl::int64 i = 0; i < number_of_elements; i++)
+      {
+	my_stack.push(random());
+      }
+
+    // gives the hint that shrinking is imminent and always prefetch 5 buffers from now on                              
     my_stack.set_prefetch_aggr(5);
-
-    for (int j = 0; j < number_of_elements - 1; j++)
-    {
-      my_stack.pop();
-    }
+    STXXL_MSG("pop...");
+    for (stxxl::int64 j = 0; j < number_of_elements - 1; j++)
+      {
+	my_stack.pop();
+      }
   }
   
   // migrating stack example with threshold 100000
