30240fe1ff29486346cc6afc22ff7640623e3998
Daniel Feist <daniel.feist@student.kit.edu>
2013-09-25 17:01:38 +0200

    Tutorial section added, vector tutorial added

--- examples/containers/vector1.cpp
+++ examples/containers/vector1.cpp
@@ -11,46 +11,40 @@
  **************************************************************************/
 
 //! [example]
-#include <iostream>
 #include <stxxl/vector>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
 
 int main()
 { 
   // template parameter <value_type, page_size, number_of_pages, block_size, alloc_strategy, paging_strategy> 
-  typedef stxxl::VECTOR_GENERATOR<int, 4, 8, 1*1024*1024, stxxl::RC, stxxl::lru>::result vector_type;  
+  typedef stxxl::VECTOR_GENERATOR<unsigned int, 4, 8, 1*1024*1024, stxxl::RC, stxxl::lru>::result vector_type;  
 
-  vector_type V;
-  int counter = 0;
-
-  srand (time(NULL));
+  vector_type my_vector;
+  unsigned int counter = 0;
+  unsigned int tmp;
+  stxxl::random_number<> rand;  
+  stxxl::uint64 number_of_elements = (long long int)(1*128) * (long long int)(1024 * 1024);
   
   // fill vector with random integers
-  for (size_t i = 0; i < 10000000; ++i) {
-    V.push_back(rand() % 100000);  
+  for (stxxl::uint64 i = 0; i < number_of_elements; ++i) {
+    tmp = rand(123456789);  // generate random number in [0,123456789)
+    my_vector.push_back(tmp);  
   }
-
-  // return size    
-  std::cout << "size of V? " << V.size() << std::endl;
-
-  // random access operator[] to receive a const reference to the element at position n                
-  const vector_type &cv = V;
-  std::cout << "V[0]=" << cv.front() << " " << ", V[n]=" << cv.back() << ", V[1234]=" << cv[1234] << " " << std::endl;
-
-  // iterate over vector from front to back and access each element
-  vector_type::const_iterator iter = V.begin();
- 
-  for (size_t j = 0; j < V.size(); ++j) {
-    //std::cout << *iter << " ";  // uncomment to observe scanning    
-    if (*iter % 2 == 0) {  // is V's current element even?
-      ++counter;
-    }    
+  
+  // construct iterator 
+  vector_type::const_iterator iter = my_vector.begin();  
+  
+  // use iterator to advance my_vector and calculate number of even elements
+  for (stxxl::uint64 j = 0; j < my_vector.size(); j++) {
+    //std::cout << *iter << " ";
+    if (*iter % 2 == 0)  // is my_vector's current element even? 
+      { 
+	++counter;
+      }    
     iter++;
   }
-   
-  std::cout << "found " << counter << " even numbers in V" << std::endl;  
-
+    
+  STXXL_MSG("found " << counter << " even numbers in V");
   return 0;
 }
 //! [example]
